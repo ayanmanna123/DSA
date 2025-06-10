5a462f3612262eed04555e9590c90006dd2156a9
Ayan Manna <ayanmanna858@gmail.com>
2025-06-10 17:16:43 +0530

    new

---
+++ day-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // cout << "hallo world " << endl ;
+    // int n;
+    // cin >> n;
+    // cout << "your value is " << n << endl;
+    // if (n > 4)
+    // {
+    //     cout << "n is gratter then 4 :" << endl;
+    // }
+    // else
+    // {
+    //     cout << " n is lass then 4 :" << endl;
+    // }
+    int n = 0;
+    for (int i = 1; i <= 10; i++)
+    {
+
+        n = n + i;
+    }
+    cout << n << endl;
+}

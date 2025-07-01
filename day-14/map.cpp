7d8f3167b4356fdb7a0dbb6e21101f1d3e2ae575
Ayan Manna <ayanmanna858@gmail.com>
2025-07-02 00:03:48 +0530

    new

---
+++ day-14/map.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    map<int, string> m2 = {{2,"ayan"}};
+    map<string ,int>m3;
+    m3["ayan"]=3;
+
+    for (auto it : m3)
+    {
+        cout << it.first << "->" << it.second << endl;
+    }
+    int arr[]={1,4,5,7,3,2};
+     
+
+}

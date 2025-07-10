a11cda60cff191fff635f017cea10c5e59bdb696
Ayan Manna <ayanmanna858@gmail.com>
2025-07-10 21:39:02 +0530

    new

--- day-19/leetcode.cpp
+++ day-19/leetcode.cpp
@@ -5,6 +5,12 @@ int main() {
      int n = 2;
         int res = 0;
         vector<int> sum(n + 1, 0);
+
+
+
+
         for (int val : sum) cout << val << " ";
+         for (int val : sum) cout << val << " ";
+          for (int val : sum) cout << val << " ";
         cout << endl;
 }
